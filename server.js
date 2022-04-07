15272f7bd975f95d96db1fc9839bcee84af82640
Amirul Sekh <sekhemarul123@gmail.com>
2022-04-07 10:36:01 +0530

    af

--- server.js
+++ server.js
@@ -1,52 +1,43 @@
-const app = require("express")()
-require("dotenv").config()
+const app = require("express")();
+require("dotenv").config();
 
-const server = require("http").createServer(app)
+const server = require("http").createServer(app);
 
-const io = require("socket.io")(server,{
-    cors:{
-        origin:"*"
-    }
-})
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-const users = { }
+const users = {};
 
-io.on("connection", (socket)=>{
-    console.log(socket.id)
-    
-    socket.on("newuser",(name)=>{
+io.on("connection", (socket) => {
+  console.log(socket.id);
 
-        users[socket.id] = name;
-    const joinmsg= name + " joined the chat"
-        //console.log(payload)
-        socket.broadcast.emit("userconnected",joinmsg)
+  socket.on("newuser", (name) => {
+    users[socket.id] = name;
+    const joinmsg = name + " joined the chat";
+    //console.log(payload)
+    socket.broadcast.emit("userconnected", joinmsg);
+    // socket.emit("joinedgroup", name)
+  });
 
-    })
+  socket.on("sendmember", (member) => {
+    io.emit("member", users);
+  });
+  socket.on("sendchat", (payload) => {
+    //console.log(payload)
+    const fullmsg = users[socket.id] + " " + payload;
+    socket.broadcast.emit("chat", fullmsg);
+  });
 
-     socket.on("sendchat",(payload)=>{
-         //console.log(payload)
-        const fullmsg = users[socket.id] +" " +payload;
-         socket.broadcast.emit("chat",fullmsg)
+  socket.on("disconnect", () => {
+    const dismsg = users[socket.id] + " " + "Disconnected";
+    socket.broadcast.emit("userdisconnected", dismsg);
+    delete users[socket.id];
+  });
+});
 
-     })
-
-     socket.on("disconnect",()=>{
-         const dismsg = users[socket.id] + " " + "Disconnected"
-         socket.broadcast.emit("userdisconnected",dismsg)
-         delete users[socket.id]
-
-    })
-
-    
-
-    
-
-
-
-
-
-})
-
-server.listen(process.env.PORT,()=>{
-    console.log("server is runnnig")
-})
+server.listen( process.env.PORT || 8000, () => {
+  console.log("server is runnnig");
+});
